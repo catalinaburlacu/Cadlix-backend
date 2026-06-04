5727c7ae80f71b4b639e7bd25fee956d75c8bac3
Lesnic Livia <livia.lesnic@mail.ru>
2026-06-04 14:03:17 +0300

    LoginDTO

---
+++ Cadlix_backend.Domain/DTOs/User/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace Cadlix_backend.Domain.DTOs.User;
+
+public class LoginDTO
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
