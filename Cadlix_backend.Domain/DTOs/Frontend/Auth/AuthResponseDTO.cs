77bd12901024f28452f5ad40158a5f011e2b2bab
Lesnic Livia <livia.lesnic@mail.ru>
2026-06-04 14:32:54 +0300

    AuthResponseDTO

---
+++ Cadlix_backend.Domain/DTOs/Frontend/Auth/AuthResponseDTO.cs
@@ -0,0 +1,11 @@
+using Cadlix_backend.Domain.DTOs;
+
+namespace Cadlix_backend.Domain.DTOs.Frontend;
+
+public class AuthResponseDto
+{
+    public UserDTO User { get; set; } = new();
+    public string Token { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+}
